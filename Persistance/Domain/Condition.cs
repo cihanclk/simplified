78737ca5fefda690fdc36f5b4b307e0df13ab6f4
Wahib Naseem <wahib1900@gmail.com>
2021-04-01 09:43:09 +0800

    Setup structure

---
+++ Persistance/Domain/Condition.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GPosProduct.Persistance.Domain
+{
+    [Table("condition")]
+    public class Condition
+    {
+        public int Id { get; set; }
+        public string Conditions { get; set; }
+        public string ColorCode { get; set; }
+    }
+}
